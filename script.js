a3ebae81f6a95a64804515fa5630a06ff844cd0a
Taiwo Omoyeni <shunlexxi112@gmail.com>
2023-02-08 19:30:47 +0100

    updates
    more images and code

--- script.js
+++ script.js
@@ -13,3 +13,34 @@ document.querySelectorAll(".navLink").forEach(n =>
         hamburger.classList.remove("active")
         hamburger.classList.remove("active")
     }))
+
+
+// img-slider
+var images = [
+    'assests/sch-building1.jpg',
+    'assests/sch-building2.jpg',
+    'assests/sch-classroom.jpg'
+];
+
+var num = 0;
+
+function next() {
+    var slider = document.getElementById('slider');
+    num++;
+
+    if(num >= images.length)
+    {
+        num = 0;
+    }
+
+    slider.src = images[num];
+}
+
+function prev() {
+    var slider = document.getElementById('slider');
+    num--;
+    if(num < 0){
+        num = images.length - 1;
+    }
+    slider.src = images[num];
+}
